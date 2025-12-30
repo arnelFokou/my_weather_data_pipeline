295c704a8c2d613c22ebf528b999ffb91636c97b
arnel <arnelfokou6@gmail.com>
2025-12-30 15:24:24 +0100

    feature: cleaning data

--- main.py
+++ main.py
@@ -15,9 +15,31 @@ def extract():
         response.raise_for_status()
     except requests.RequestException as e:
         raise RuntimeError(f"Erreur lors de l'appel API : {e}")
-
-
     
+    return response.json()
 
+def transform(data):
+    output_data ={}
 
-print(extract())
+    output_data["longitude"]    = data['coord'] ['lon']
+    output_data["latitude"]     = data['coord'] [ 'lat']    
+    output_data["description"]  = data['weather'] [0] ['description']
+
+    output_data["temp"]         = data['main'] ['temp']
+    output_data["feel_temp"]    = data['main'] ['feels_like']
+    output_data["humidity"]     = data['main'] ['humidity']
+    output_data["wind_speed"]   = int(data['wind'] ['speed']) * 3.6 # convert from m/s to km/h
+
+    output_data["country"]      = data['sys'] ['country']
+    output_data["city_name"]    = data['name']
+
+    output_data["dt"]           = dt.fromtimestamp(data['dt']).strftime('%Y-%m-%d %H:%M:%S')
+    output_data['extract_date'] = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    df = pd.DataFrame([output_data],index=None) 
+    name_file = output_data['extract_date'].replace(" ","_").split(":")[0] 
+    df.to_csv(f"extraction_{name_file}.csv")
+
+data = extract()
+
+transform(data)
